b797d297b28d326a6c58e4d0a7e1c7a98948eeec
Rick <rick@rick.home>
2021-02-22 17:25:57 +0100

    Add nav component

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,13 +1,16 @@
 import styles from './Header.module.css';
-import Nav from './Nav';
+import Nav from '../Nav';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <div className={styles.container}>
       <div>
-        <h2>
-          Seed Library
-        </h2>
+        <Link href="/">
+          <h2 className={styles.containertitle}>
+            Seed Library
+          </h2>
+        </Link>
       </div>
     
     <div>
@@ -15,6 +18,6 @@ const Header = () => {
     </div>
     </div>
   )
-}
+};
 
-export default Header
+export default Header;
